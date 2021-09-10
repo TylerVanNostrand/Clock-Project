ce160459d3e72faa772249319f1ba535e60e8a22
tvannost <tvannost@hotmail.com>
2021-09-10 10:10:19 -0400

    added JS code, and made notations

---
+++ js/main.js
@@ -0,0 +1,42 @@
+function Time() {
+ 
+ var date = new Date(); // Creating object of the Date class
+ var hour = date.getHours(); // Get current hour
+ var minute = date.getMinutes(); // Get current minute 
+ var second = date.getSeconds(); // Get current second
+ var period = ""; // Variable to store AM / PM
+ 
+ if (hour >= 12) {
+ period = "PM";
+ } else {
+ period = "AM";
+ } // Assigning AM / PM according to the current hour
+ 
+ if (hour == 0) {
+ hour = 12;
+ } else {
+ if (hour > 12) {
+ hour = hour - 12;
+ }
+ } 
+ 
+ 
+ hour = update(hour); // Converting the hour in 12-hour format
+ minute = update(minute); // Updating hour, minute, and second
+ second = update(second); // if they are less than 10
+ 
+ document.getElementById("digital-clock").innerText = hour + " : " + minute + " : " + second + " " + period; // Adding time elements to the div
+ 
+ setTimeout(Time, 1000); // Set Timer to 1 sec (1000 ms)
+}
+ // Function to update time elements if they are less than 10
+ // Append 0 before time elements if they are less than 10
+function update(t) {
+ if (t < 10) {
+ return "0" + t;
+ }
+ else {
+ return t;
+ }
+}
+Time();
--- psuedocode.md
+++ psuedocode.md
@@ -1,5 +1,5 @@
 This is where I will be planningo out my psuedocode
 
 Step one will be setting up the html for the project. Will need a working clock, time, date, and alarm.
-Step two will be setting up the CSS sheet to make clock look nice.
-Step three will be setting up the JS.
+Step two will be setting up the JS to make clock function.
+Step three will be setting up the CSS sheet to make clock look nice.
