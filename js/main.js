126a660274b2be6bf533aaf1b689440919daba61
tvannost <tvannost@hotmail.com>
2021-09-10 11:27:02 -0400

    added div's and var

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 function Time() {
+ var clockDiv = document.getElementById("clock");
+ var dateDiv = document.getElementById("date");
+ var date = new Date(); // Sets the value of the current date
  
- var date = new Date(); // Creating object of the Date class
  var hour = date.getHours(); // Get current hour
  var minute = date.getMinutes(); // Get current minute 
  var second = date.getSeconds(); // Get current second
  var period = ""; // Variable to store AM / PM
  
+ var month = date.getMonth()+1; // Get the current month
+ var day = date.getDate(); // Get the current day
+ var year = date.getFullYear(); // Get current year
+ 
  if (hour >= 12) {
- period = "PM";
+ period = "PM"; // Assigning AM / PM according to the current hour
  } else {
  period = "AM";
- } // Assigning AM / PM according to the current hour
+ } 
  
  if (hour == 0) {
  hour = 12;
@@ -29,14 +35,16 @@ function Time() {
  
  setTimeout(Time, 1000); // Set Timer to 1 sec (1000 milliseconds)
 }
- // Function to update time elements if they are less than 10
- // Append 0 before time elements if they are less than 10
-function update(t) {
- if (t < 10) {
+ 
+function update(t) { 
+ if (t < 10) { // Function to update time elements if they are less than 10
  return "0" + t;
  }
- else {
+ else { // Append 0 before time elements if they are less than 10
  return t;
  }
 }
 Time();
+
+var alarmTime = null; // sets value of alarmTime to 0
+var alarmTimeout = null; // sets value of alarmTimeout to 0
